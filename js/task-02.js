f81f52f54dc0e5eb38a7374323b2491825008557
Yurii Khomin <yurii.khomin@gmail.com>
2021-04-08 05:35:36 +0300

    update

---
+++ js/task-02.js
@@ -0,0 +1,12 @@
+const ingredients = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',
+  ];
+
+const ingredientsRef = document.querySelector('#ingredients');
+
+const ulList = document.createElement('li');
