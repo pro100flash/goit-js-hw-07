e457f19d9a9b7ab12a93200eca8b78cb2c052366
Yurii Khomin <yurii.khomin@gmail.com>
2021-04-12 01:59:19 +0300

    task 5 done

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,3 @@
-// Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), 
-// подставляет его текущее значение в span#name-output. 
-// Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
-
 const inputValue = document.getElementById('name-input');
 const outputValue = document.getElementById('name-output');
 
