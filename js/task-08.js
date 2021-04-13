db9f69188d5dc53387358f3335d304a463a23a42
Yurii Khomin <yurii.khomin@gmail.com>
2021-04-13 03:45:29 +0300

    task 8 in progress...

---
+++ js/task-08.js
@@ -0,0 +1,27 @@
+const refs = {
+    render: document.querySelector('button[data-action="render"]'),
+    destroy:  document.querySelector('button[data-action="destroy"]'),
+    boxes: document.getElementById('boxes'),
+    input: document.getElementById('controls input')
+};
+
+function createBoxes(amount) {
+    let newArray = [];
+
+    for (let i = 0; i < amount; i +=1) {
+        const newDiv = document.createElement('div');
+        newDiv.style.width = `${30 + i * 10}px`;
+        newDiv.style.height = `${30 + i * 10}px`;
+        newDiv.style.backgroundColor =
+        '#' + (((1 << 24) * Math.random()) | 0).toString(16);
+        newArray.push(newDiv);
+    }
+    refs.boxex.append(...newArray);
+};
+
+function destroyBoxes() {
+    refs.boxes.innerHTML = '';
+};
+
+refs.render.addEventListener('click', () => createBoxes(refs.input.value));
+refs.destroy.addEventListener('click', destroyBoxes);
